7e67b595fe5f412d7f109c89f7ec027b30ba2303
Dave Melia <retrospock@gmail.com>
2014-08-11 11:25:02 +0100

    MaxLength condition added to input
    Added a maxLength variable set to 600 to limit the array input.

--- func.js
+++ func.js
@@ -5,11 +5,13 @@ Array.prototype.toSingleQuotedString = function () {
 
 // Main function to ammend inputted strings
 function change() {
+
     var string_input = (document.getElementById('string_input')),
         stringArray = string_input.value.replace(/^\s+|\s+$/g, '').split(/\s+/),
         radio_comma = document.getElementById("radioComma"),
         radio_singlequote = document.getElementById("radioSingleQuote"),
         radio_dash = document.getElementById("radioDash");
+        maxLength = 600;
 
     if (radio_comma.checked) {
         string_output = stringArray.join(radio_comma.value);
@@ -22,7 +24,12 @@ function change() {
     }
 
     // Displays formatted string
-    document.getElementById("string_output").value = (string_output);
+    if(stringArray.length <= maxLength){
+        document.getElementById("string_output").value = (string_output);
+    } else {
+        document.getElementById("string_output").value = "";
+        document.getElementById("string_output").value = "Input limited to " + maxLength + ".  Please reduce and try again.";
+    }
 };
 
 
